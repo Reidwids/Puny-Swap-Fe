eb22002474f2457ddcc69aca9b0d3de7c79b3703
reidwids <96877197+Reidwids@users.noreply.github.com>
2022-04-22 11:58:10 -0400

    Metamask send and receive working

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,9 +7,9 @@ export const About = () => {
   const {sendTransaction, connectWallet, currentAccount, formData, handleChange, setFormData} = useContext(TransactionContext);
 
   const handleSubmit = (e) =>{
-    const {addressTo, amount, keyword, message} = formData;
+    // const {addressTo, amount, keyword, message} = formData;
     e.preventDefault();
-    sendTransaction();
+    sendTransaction(formData);
   }
 
   return (
@@ -25,16 +25,16 @@ export const About = () => {
           <a href='http://localhost:3000/signup' className='btn'>Get Started</a>
           <Container>
               <Form.Group>
-                  <Form.Control required name='addressTo' placeholder='Address To' onChange={handleChange}></Form.Control>
+                  <Form.Control required name='addressTo' placeholder='Address To' onChange={(e)=>handleChange(e, 'addressTo')}></Form.Control>
               </Form.Group>
               <Form.Group>
-                  <Form.Control required name='amount' placeholder='Amount (Eth)'onChange={handleChange}></Form.Control>
+                  <Form.Control required name='amount' placeholder='Amount (Eth)'onChange={(e)=>handleChange(e, 'amount')}></Form.Control>
               </Form.Group>
               <Form.Group>
-                  <Form.Control required name='keyword' placeholder='Keyword (Gif)'onChange={handleChange}></Form.Control>
+                  <Form.Control required name='keyword' placeholder='Keyword (Gif)'onChange={(e)=>handleChange(e, 'keyword')}></Form.Control>
               </Form.Group>
               <Form.Group>
-                  <Form.Control required name='message' placeholder='Message' onChange={handleChange}></Form.Control>
+                  <Form.Control required name='message' placeholder='Message' onChange={(e)=>handleChange(e, 'message')}></Form.Control>
               </Form.Group>
               <Button varient='primary' onClick={handleSubmit}>Send Now</Button>
           </Container>
